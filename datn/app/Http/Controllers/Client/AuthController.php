d723b347cdd8b464a20afe96f5ab05758673adde
nguyenminhtien2504 <62551432+nguyenminhtien2504@users.noreply.github.com>
2021-04-15 12:38:02 +0700

    validate đăng ký

--- datn/app/Http/Controllers/Client/AuthController.php
+++ datn/app/Http/Controllers/Client/AuthController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistrationRequest;
-use Illuminate\Foundation\Auth\User;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
 use Illuminate\Contracts\Auth\Authenticatable;
@@ -34,8 +35,9 @@ class AuthController extends Controller
         Session::flush(); // xóa hết các session khác
         return redirect()->route('client.login'); // chuyển về trang đăng nhập
     }
-    public function registration(Request $request){
+    public function registration(RegistrationRequest $request){
         $data = $_POST;
+
         $user = new User();
         $user->fill($data);
         if($request->hasFile('avatar')){
@@ -46,6 +48,7 @@ class AuthController extends Controller
         $user->role_id = 0;
         $user->status = 0;
         $user->coins = 0;
+        
         $user->save();
         Session::put('message','Thêm tài khoản thành công');
         return redirect()->route('client.login'); 
--- datn/app/Http/Requests/RegistrationRequest.php
+++ datn/app/Http/Requests/RegistrationRequest.php
@@ -24,12 +24,10 @@ class RegistrationRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'required',
+                'name'=>'required',
                 'avatar'=>'required|image',
-                'role_id'=>'required',
                 'email'=>'required|email|max:255|unique:users',
                 'phone'=>'required|min:10|numeric',
-                'status'=>'required|min:1',
                 'address'=>'required',
                 'password' => 'required|string|min:6|confirmed|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/',
                 'password-confirm'=>'requied|min4',
@@ -41,12 +39,10 @@ class RegistrationRequest extends FormRequest
             'name.required' =>'Bạn cần nhập Tên tài khoản',
                 'avatar.required'=>'Không để trống ảnh của user',
                 'avatar.image'=>'Ảnh phải có đuôi là file(jpeg, png, bmp, gif, or svg)',
-                'role_id.required'=>'Chọn trạng thái hoạt động',
                 'email.required'=>'Vui lòng nhập Email',
                 'email.unique'=>'Email đã tồn tại',
                 'phone.required'=> 'Nhập số điện thoại',
                 'phone.min'=> 'Nhập số điện thoại có 10 chữ số',
-                'status.required'=>'Chọn trạng thái',
                 'password.required' => 'Bạn cần nhập mật khẩu ',
                 'password.min'=>'Mật khẩu phải có chữ hoa chữ thường và số từ 6 đến 16 kí tự',
                 'password.max'=>'Mật khẩu phải có chữ hoa chữ thường và số từ 6 đến 16 kí tự',
